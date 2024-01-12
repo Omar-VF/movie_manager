700935e53f53b79fa3ac1d7f7c3834ca8085d999
Omar <omarvfais@gmail.com>
2024-01-12 20:55:37 +0530

    Poster feature added to list, Db changed to postgresql.

--- movies/models.py
+++ movies/models.py
@@ -6,6 +6,10 @@ class movie_data(models.Model):
     title=models.CharField(max_length=250)
     year=models.IntegerField(null=True)
     summary=models.TextField(blank=True)
+    poster=models.ImageField(upload_to='images/',null=True)
+
+    def __str__(self):
+        return self.title
 
 class Director(models.Model):
     name=models.CharField(max_length=300)
--- movies/views.py
+++ movies/views.py
@@ -6,11 +6,11 @@ from . forms import movie_form
 def create(request):
     frm=movie_form()
     if request.POST:
-        frm=movie_form(request.POST)
+        frm=movie_form(request.POST,request.FILES)
         if frm.is_valid:
             frm.save()
-        else:
-            frm=movie_form()
+    else:
+        frm=movie_form()
 
 
     return render(request,'create.html',{'frm':frm})
@@ -22,11 +22,11 @@ def list(request):
 def edit(request,pk):
     edit_instance=movie_data.objects.get(pk=pk)
     if request.POST:
-        frm=movie_form(request.POST,instance=edit_instance)
+        frm=movie_form(request.POST,request.FILES,instance=edit_instance)
         if frm.is_valid():
             edit_instance.save()
-        else:
-            edit_instance=movie_form(instance=edit_instance)
+    else:
+        edit_instance=movie_form(instance=edit_instance)
     frm=movie_form(instance=edit_instance)
 
         
