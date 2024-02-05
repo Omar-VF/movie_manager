bb4c616c492fef8533e330a28d2c8d084c7a8eac
Omar <omarvfais@gmail.com>
2024-02-05 21:37:34 +0530

    movie age rating

--- movies/models.py
+++ movies/models.py
@@ -2,15 +2,29 @@ from django.db import models
 
 # Create your models here.
 
+
+class censor_info(models.Model):
+    rating = models.CharField(max_length=10)
+    
+    def __str__(self):
+        return self.rating
+
+
 class movie_data(models.Model):
-    title=models.CharField(max_length=250)
-    year=models.IntegerField(null=True)
-    summary=models.TextField(blank=True)
-    poster=models.ImageField(upload_to='images/',null=True,blank=True,default='UnavailableImg.png')
-
-
+    title = models.CharField(max_length=250)
+    year = models.IntegerField(null=True)
+    summary = models.TextField(blank=True)
+    poster = models.ImageField(
+        upload_to="images/", null=True, blank=True, default="UnavailableImg.png"
+    )
+    censor_details = models.OneToOneField(
+        censor_info, on_delete=models.SET_NULL, related_name="movie", null=True, blank=True
+    )
+    def __str__(self) -> str:
+        return self.title
     class Meta:
-        ordering=('title',)
+        ordering = ("title",)
+
 
 class Director(models.Model):
-    name=models.CharField(max_length=300)
+    name = models.CharField(max_length=300)
--- movies/views.py
+++ movies/views.py
@@ -1,40 +1,43 @@
-from django.shortcuts import render,redirect
-from . models import movie_data
-from . forms import movie_form
+from django.shortcuts import render, redirect
+from .models import movie_data
+from .forms import movie_form
+
 # Create your views here.
 
+
 def create(request):
-    frm=movie_form()
+    frm = movie_form()
     if request.POST:
-        frm=movie_form(request.POST,request.FILES)
+        frm = movie_form(request.POST, request.FILES)
         if frm.is_valid:
             frm.save()
-            return redirect('list')
+            return redirect("list")
     else:
-        frm=movie_form()
+        frm = movie_form()
 
+    return render(request, "create.html", {"frm": frm})
 
-    return render(request,'create.html',{'frm':frm})
 
 def list(request):
-    movie_info=movie_data.objects.all()
-    return render(request,'list.html',{'movies':movie_info})
+    movie_info = movie_data.objects.all()
+    return render(request, "list.html", {"movies": movie_info})
 
-def edit(request,pk):
-    edit_instance=movie_data.objects.get(pk=pk)
+
+def edit(request, pk):
+    edit_instance = movie_data.objects.get(pk=pk)
     if request.POST:
-        frm=movie_form(request.POST,request.FILES,instance=edit_instance)
+        frm = movie_form(request.POST, request.FILES, instance=edit_instance)
         if frm.is_valid():
             edit_instance.save()
-            return redirect('list')
+            return redirect("list")
         else:
-            edit_instance=movie_form(instance=edit_instance)
-    frm=movie_form(instance=edit_instance)
+            edit_instance = movie_form(instance=edit_instance)
+    frm = movie_form(instance=edit_instance)
 
-        
-    return render(request,'create.html',{'frm':frm})
+    return render(request, "create.html", {"frm": frm})
 
-def delete(request,pk):
-    del_instance=movie_data.objects.get(pk=pk)
+
+def delete(request, pk):
+    del_instance = movie_data.objects.get(pk=pk)
     del_instance.delete()
-    return redirect('list')
+    return redirect("list")
