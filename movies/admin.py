bddc0e91e15f551f0e2aa7003c3be332e9594ea4
Omar <omarvfais@gmail.com>
2024-02-07 01:07:20 +0530

    db additions

--- movies/admin.py
+++ movies/admin.py
@@ -3,5 +3,6 @@ from . models import *
 # Register your models here.
 
 admin.site.register(movie_data)
-admin.site.register(Director)
+admin.site.register(director)
 admin.site.register(censor_info)
+admin.site.register(actors)
--- movies/models.py
+++ movies/models.py
@@ -5,9 +5,23 @@ from django.db import models
 
 class censor_info(models.Model):
     rating = models.CharField(max_length=10)
-    
+
     def __str__(self):
         return self.rating
+
+
+class director(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class actors(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class movie_data(models.Model):
@@ -17,14 +31,22 @@ class movie_data(models.Model):
     poster = models.ImageField(
         upload_to="images/", null=True, blank=True, default="UnavailableImg.png"
     )
-    censor_details = models.OneToOneField(
-        censor_info, on_delete=models.SET_NULL, related_name="movie", null=True, blank=True
+    censor_details = models.ForeignKey(
+        censor_info,
+        on_delete=models.SET_NULL,
+        related_name="movie",
+        null=True,
+        blank=True,
     )
+
+    directed_by = models.ForeignKey(
+        director, null=True, on_delete=models.CASCADE, related_name="directed_by"
+    )
+
+    actor = models.ManyToManyField(actors, related_name="acted_movies")
+
     def __str__(self) -> str:
         return self.title
+
     class Meta:
         ordering = ("title",)
-
-
-class Director(models.Model):
-    name = models.CharField(max_length=300)
